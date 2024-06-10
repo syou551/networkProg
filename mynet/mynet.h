a270eb6c9160188caed822f25ea19df85f4ad60a
syou551 <hiroto.0108si@gmail.com>
2024-06-10 04:24:12 +0000

    add udp lib

--- mynet/mynet.h
+++ mynet/mynet.h
@@ -16,4 +16,13 @@
 int init_tcpserver(in_port_t myport, int backlog);
 int init_tcpclient(char *servername, in_port_t serverport);
 void exit_errmesg(char *errmesg);
+//udp
+int init_udpserver(in_port_t myport);
+int init_udpclient();
+void set_sockaddr_in(struct sockaddr_in *server_adrs, 
+		     char *servername, in_port_t port_number );
+int Sendto( int sock, const void *s_buf, size_t strsize, int flags, 
+	    const struct sockaddr *to, socklen_t tolen);
+int Recvfrom(int sock, void *r_buf, size_t len, int flags,
+	     struct sockaddr *from, socklen_t *fromlen);
 #endif  /* MYNET_H_ */
